8124c73bd57c98fa83ec6df453e5a172df7eb36f
songseul <song.codestates@gmail.com>
2022-08-22 14:44:09 +0900

    solo project 과제 제출1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const ChangeValueButton = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #61dafb;
+  font-size: 36px;
+  border: 0;
+  border-radius: 50%;
+  width: 4rem;
+  height: 4rem;
+  margin: 1rem;
+`;
+
+const ButtonWrapper = styled.div`
+  margin: 1rem;
+`;
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>더하기 빼기</p>
+        <span>{count} </span>
+        <ButtonWrapper>
+          <ChangeValueButton
+            aria-label="Increment value"
+            onClick={() => setCount(count + 1)}
+          >
+            +
+          </ChangeValueButton>
+          <ChangeValueButton
+            aria-label="Decreament value"
+            onClick={() => setCount(count - 1)}
+          >
+            -
+          </ChangeValueButton>
+        </ButtonWrapper>
       </header>
     </div>
   );
